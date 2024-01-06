73b69b83dbefb4fc5f8d4b4e0d457ae84f3fa6a4
lbdoriana <lbdoriana@gmail.com>
2024-01-06 04:50:43 +0100

    second commit

--- script.js
+++ script.js
@@ -15,8 +15,47 @@ function getWeatherData() {
       document.getElementsByClassName("temp")[0].innerHTML = `${Math.round(data.main.temp)}°C`;
       document.getElementsByClassName("humidity")[0].innerHTML = `${data.main.humidity}%`;
       document.getElementsByClassName("wind-speed")[0].innerHTML = `${data.wind.speed} m/s`;
+      
     })
-}
+    .catch((error) => {
+      console.error(error);
+      alert(`Erreur : ${error.message}`);
+      res.status(500).send('');
+    });
+    }
+
+    function getImageUrl(description) {
+      // Ajouter des images correspondant à chaque description
+      switch (description) {
+        case "ciel dégagé":
+          return "images.jpeg";
+        case "peu nuageux":
+          return "https://i.imgur.com/4yjyJvB.jpg";
+        case "nuages épars":
+          return "https://i.imgur.com/4yjyJvB.jpg";
+        case "couvert":
+          return "https://i.imgur.com/4yjyJvB.jpg";
+        case "pluie légère":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "pluie modérée":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "pluie forte":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "neige légère":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "neige modérée":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "neige forte":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        case "orage":
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+        default:
+          return "https://i.imgur.com/7J9WZ8t.jpg";
+      }
+    }
+    description()
+
+
 
 
 
